adb792008d7377f7586acc166d850a725dccbfbd
Benjamin Egers <b.egers@unasat.sr>
2023-09-02 02:47:41 -0300

    btnStyles

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        // API_URL: "http://localhost:8000/",
-        API_URL: "https://collaboratio-api.vercel.app/api/"
+        API_URL: "http://localhost:8000/",
+        // API_URL: "https://collaboratio-api.vercel.app/api/"
     }
 }
 
